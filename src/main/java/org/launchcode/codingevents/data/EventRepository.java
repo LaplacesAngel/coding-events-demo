4c72f494c610ae7757e4abc1c6157d45ffc47d50
paul <paulentz@gmail.com>
2023-05-21 23:51:26 -0500

    not working fixed with: spring.jpa.hibernate.hbm2ddl.auto=update changed it back to spring.jpa.hibernate.ddl-auto = update Caused by: javax.persistence.PersistenceException: [PersistenceUnit: default] Unable to build Hibernate SessionFactory; nested exception is org.hibernate.exception.SQLGrammarException: Unable to open JDBC Connection for DDL execution

--- src/main/java/org/launchcode/codingevents/data/EventRepository.java
+++ src/main/java/org/launchcode/codingevents/data/EventRepository.java
@@ -1,11 +1,10 @@
 package org.launchcode.codingevents.data;
 
+import org.launchcode.codingevents.models.Event;
 import org.launchcode.codingevents.models.Events;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventRepository extends CrudRepository <Events, Integer>{
-
-
+public interface EventRepository extends CrudRepository <Event, Integer>{
 }
