717e7a38e174775ee10e182ceaa85723f13e924a
paul <paulentz@gmail.com>
2023-05-28 16:33:01 -0500

    got one to many working

--- src/main/java/org/launchcode/codingevents/data/EventRepository.java
+++ src/main/java/org/launchcode/codingevents/data/EventRepository.java
@@ -1,7 +1,6 @@
 package org.launchcode.codingevents.data;
 
 import org.launchcode.codingevents.models.Event;
-import org.launchcode.codingevents.models.Events;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/org/launchcode/codingevents/models/EventDetails.java
+++ src/main/java/org/launchcode/codingevents/models/EventDetails.java
@@ -5,11 +5,35 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class EventDetails {
-    @Id
-    @GeneratedValue
-    private int id;
+public class EventDetails extends AbstractEntity{
+
     private String contactEmail;
 
     private String description;
+
+    public EventDetails(String contactEmail, String description) {
+        this.contactEmail = contactEmail;
+        this.description = description;
+    }
+
+    public EventDetails() {
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
 }
